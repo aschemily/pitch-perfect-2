d47689782deb6767968fe6abe6843fa62b012868
Emily A <aschemily@gmail.com>
2021-05-25 16:49:28 -0400

    set up playviewcontroller storyboard and created iboutlet and ibaction

--- pitch perfect 2/ViewController.swift
+++ pitch perfect 2/ViewController.swift
@@ -9,10 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        stopButton.isEnabled = false
+        print("starting over")
     }
+    
+    @IBAction func recordingOn(_ sender: Any) {
+        recordButton.isEnabled = false
+        stopButton.isEnabled = true
+        print("button was pressed")
+    }
+    
+    @IBAction func recordingStopped(_ sender: Any) {
+        recordButton.isEnabled = true
+        stopButton.isEnabled = false
+        print("button was stopped")
+    }
+    
+    
 
 
 }
